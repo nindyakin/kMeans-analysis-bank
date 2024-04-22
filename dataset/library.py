cade146a5ba92a4ac7b048f010d45d9027c1e8a1
Nindya Kinanti <69761644+nindyakin@users.noreply.github.com>
2024-04-22 16:17:58 +0700

    Update library.py

--- dataset/library.py
+++ dataset/library.py
@@ -1,12 +1,12 @@
 # Import library
 
 import gdown
-import pandas as pd                 # Library used for working with data sets and perform data analysis
-import numpy as np                  # To perform mathematical operasion and statistics
-from datetime import datetime, date # Used to perform dates and time manipulation
-import seaborn as sns               # To perform data visualization
-import matplotlib.pyplot as plt     # To perform data visualization
-import shutil                       # To download and unzip google drive data
+import pandas as pd                                                 # Library used for working with data sets and perform data analysis
+import numpy as np                                                  # To perform mathematical operasion and statistics
+from datetime import datetime, date                                 # Used to perform dates and time manipulation
+import seaborn as sns                                               # To perform data visualization
+import matplotlib.pyplot as plt                                     # To perform data visualization
+import shutil                                                       # To download and unzip google drive data
 import seaborn as sns
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
