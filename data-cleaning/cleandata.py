7bcf85eed3dd645bddaa81fb8948900e4de54814
Nindya Kinanti <69761644+nindyakin@users.noreply.github.com>
2024-04-22 16:37:05 +0700

    Update cleandata.py

--- data-cleaning/cleandata.py
+++ data-cleaning/cleandata.py
@@ -1,3 +1,8 @@
 # Mengetahui shape dari dataset
-
 df_bankpromo.shape
+
+# Mengetahui info nama column dan data type di dataset
+df_bankpromo.info()
+
+# Mengubah data type Account id
+df_bankpromo['ACCOUNT_ID'] = df_bankpromo['ACCOUNT_ID'].astype(str)
